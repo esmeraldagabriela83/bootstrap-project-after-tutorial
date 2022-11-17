f13d60a6584ed00a31f60f4e0b1d9a9b1e0ff4a3
Acornicesei Mihaela Gabriela <esmeraldagabriela83@yahoo.com>
2022-11-17 22:22:42 +0200

    bootstrap project start

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+
+function App(){
+  return(
+    <>
+bootstrap text
+
+<p id="coloredText">colored text</p>
+
+    </>
+  );
+}
+
+ReactDOM.render(<App/> , document.getElementById('app'));
